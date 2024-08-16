f51e615b96ff643f214607838e6f0e9d16ec5b3e
lwx <lwx@macbookair>
2024-08-16 18:54:33 +0800

    change to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import CodeEditor from "./pages/code-editor.page";
 import NewEditorPage from "./pages/new-editor.page";
